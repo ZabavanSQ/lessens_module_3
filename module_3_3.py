98019fb6c68ac0efa56fb236bebf679cd2f47436
ZabavanSQ <zabavnsq@mail.ru>
2024-09-07 15:08:29 +0300

    Распаковка

---
+++ module_3_3.py
@@ -0,0 +1,13 @@
+
+def print_params(a=1, b='строка', c=True):
+    print(a, b, c)
+
+print_params(a = 25)
+print_params(c = [1, 2,3])
+print_params()
+
+values_list_2=[54.32, 'строка']
+print_params(*values_list_2, 42)
+
+values_dict={'a': 123, 'b':'вертикаль', 'c': 12.34 }
+print_params(**values_dict)
