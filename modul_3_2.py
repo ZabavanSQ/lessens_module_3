9b1fb6630cb635b76ca7dc59f5d527dfb3697aac
ZabavanSQ <zabavnsq@mail.ru>
2024-09-07 13:51:54 +0300

    Add files via upload

--- modul_3_2.py
+++ modul_3_2.py
@@ -1,16 +1,19 @@
 def send_email(message, recipient, sender = "university.help@gmail.com"):
-    while recipient == sender:
+
+
+    if recipient == sender:
         print("Нельзя отправить письмо самому себе!")
-        break
-    if '@' in recipient and  '.com'in recipient or '.ru' in recipient or '.net' in recipient:
-        if '@' in sender and  '.com'in sender or '.ru' in sender or '.net' in sender:
+    else:
+        if '@' in sender and '.com' in sender or '.ru' in sender or '.net' in sender:
+            if '@' in recipient and '.com' in recipient:
                 print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}")
+            else:
+                if '.ru' in recipient or '.net' in recipient:
+                    print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}")
+                else:
+                    print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
         else:
             print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
-    else:
-         print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
-
-
 
 
 
